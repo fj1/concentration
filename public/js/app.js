5025a36cb561ef64a6f1da2195fa83830b82e00f
Frances Jurek <jurek.frances@gmail.com>
2014-08-29 21:18:39 -0500

    card deck displays on game board when shuffle button is clicked

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,71 @@
+var cards = [
+  { 
+    "value": "1",
+    "imgUrl": "kitten1.jpg"
+  },
+  { 
+    "value": "2",
+    "imgUrl": "public/images/kitten1.jpg"
+  },
+  { 
+    "value": "3",
+    "imgUrl": "public/images/kitten2.jpg"
+  },
+  { 
+    "value": "4",
+    "imgUrl": "public/images/kitten2.jpg"
+  },
+  { 
+    "value": "5",
+    "imgUrl": "public/images/kitten3.jpg"
+  },
+  { 
+    "value": "6",
+    "imgUrl": "public/images/kitten3.jpg"
+  },
+  { 
+    "value": "7",
+    "imgUrl": "public/images/kitten4.jpg"
+  },
+  { 
+    "value": "8",
+    "imgUrl": "public/images/kitten4.jpg"
+  },
+  { 
+    "value": "9",
+    "imgUrl": "public/images/kitten5.jpg"
+  },
+  { 
+    "value": "10",
+    "imgUrl": "public/images/kitten5.jpg"
+  },
+  { 
+    "value": "11",
+    "imgUrl": "public/images/kitten6.jpg"
+  },
+  { 
+    "value": "12",
+    "imgUrl": "public/images/kitten6.jpg"
+  }
+]
 
-// // display cards on board
-// $(document).ready(function() {
-//   $('.card').each.html("<img src='public/back.jpg'>")
-// });
+// display cards on board on shuffle button
+// TO-DO: simulate shuffle button click on page load?
+$('.shuffleButton').click(function() {
+    for (var i = 0; i < 12; i++) {
+      var pic = cards[i].imgUrl;
+      var currentCard = $('.card:eq(' + i + ')')
+      // console.log("currentCard is ", currentCard);
+      currentCard.html("<img src='" + pic + "'>");
+    }
+});
+
+
+
+
+
+
+
+
+
+

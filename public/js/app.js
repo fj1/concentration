1e1d93cdfb5af5450536eb88c3ffb01876f7e7b5
Frances Jurek <jurek.frances@gmail.com>
2014-08-29 22:37:12 -0500

    modularized shuffle/deal functions, mocked out gameOn function

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 var cards = [
   { 
     "value": "1",
-    "imgUrl": "kitten1.jpg"
+    "imgUrl": "public/images/kitten1.jpg"
   },
   { 
     "value": "2",
@@ -49,36 +49,48 @@ var cards = [
   }
 ]
 
-// display cards on board on shuffle button
-// TO-DO: simulate shuffle button click on page load?
-$('.shuffleButton').click(function() {
-  // Fisher-Yates shuffle
-  shuffle(cards);
-  function shuffle(array) {
-    var m = array.length
-    var t;
-    var i;
-    // While there remain elements to shuffle...
-    while (m) {
-      // Pick a remaining element...
-      i = Math.floor(Math.random() * m--);
-      // And swap it with the current element.
-      t = array[m];
-      array[m] = array[i];
-      array[i] = t;
-    }
-    console.log("array is now", array);
-    return array;
+// ************ SHUFFLE ALGORITHM ************
+// Fisher-Yates shuffle
+function shuffle(array) {
+  var m = array.length
+  var t;
+  var i;
+  // While there remain elements to shuffle...
+  while (m) {
+    // Pick a remaining element...
+    i = Math.floor(Math.random() * m--);
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
   }
+  return array;
+}
 
-  // put cards on board
+// ************ DEAL CARDS ************
+function dealCards() {
   for (var i = 0; i < 12; i++) {
     var pic = cards[i].imgUrl;
-    var currentCard = $('.card:eq(' + i + ')')
-    // console.log("currentCard is ", currentCard);
+    var currentCard = $('.card:eq(' + i + ')');
     currentCard.html("<img src='" + pic + "'>");
   }
+}
+
+// TO-DO: simulate shuffle button click on page load?
+// ************ SHUFFLE BUTTON ************
+$('.shuffleButton').click(function() {
+  shuffle(cards);
+  dealCards();
+  // play game
+  gameOn();
 });
+
+// ************ GAME PLAY ************
+function gameOn() {
+  // $('.card').click ---> show card
+  // second $('.card').click ---> show 2nd card
+  // compare cards
+}
 
 
 
